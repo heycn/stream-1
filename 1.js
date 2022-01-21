1eec383a89c1b39980a473bc726caa02e6c27e35
heycn <x1chennan@qq.com>
2022-01-21 20:45:13 +0800

    update\

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const stream = fs.createWriteStream('./big_file.txt')
 
-for (let i = 0; i < 1000000; i++) {
+for (let i = 0; i < 10000; i++) {
   stream.write(`这是第 ${i} 行内容，要不停写文件，回车\n`)
 }
 
